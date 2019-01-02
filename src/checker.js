8176871e77593c849213036c2a63a400d5ecdbde
Fei <fyang1024@gmail.com>
2019-01-03 08:41:15 +1100

    add logic to update game status for pending games before check block for them

--- src/checker.js
+++ src/checker.js
@@ -4,15 +4,18 @@ import { list, update } from './game.js';
 
 const config = require('./config.js');
 const args = require('yargs').argv;
-const contracts = require('./contracts.json');
 
 if (!args.network) {
   console.error('network is not specified. e.g. --network=development');
   process.exit();
 }
 
-const network = config.networks[args.network];
+if (!process.env.endPoint || !process.env.jwt) {
+  console.error('Environment variables are missing');
+  process.exit();
+}
 
+const network = config.networks[args.network];
 const tronWeb = new Tronweb(
   network.fullHost,
   network.fullHost,
@@ -20,30 +23,17 @@ const tronWeb = new Tronweb(
   network.privateKey
 );
 
+const contracts = require('./contracts.json');
 const factory = contracts['FaceWorthPollFactory'];
-
 const factoryContract = tronWeb.contract(factory.abi, factory.address);
 
 function check(hash, block) {
-  tronWeb.trx.getBalance(tronWeb.defaultAddress.hex).then((err, balance) => {
-    console.log('Balance before check', balance / 1000000);
-  });
-  tronWeb.trx.getBandwidth(tronWeb.defaultAddress.hex).then((err, bandwidth) => {
-    console.log('Bandwidth before check', bandwidth);
-  });
   console.log(new Date(), 'Checking Block Number for ', hash, 'at block', block);
   factoryContract.checkBlockNumber(`0x${hash}`).send({
     shouldPollResponse: false,
     callValue: 0,
     feeLimit: 1000000000
-  }).then(()=> {
-    tronWeb.trx.getBalance(tronWeb.defaultAddress.hex).then((err, balance) => {
-      console.log('Balance after check', balance / 1000000);
-    });
-    tronWeb.trx.getBandwidth(tronWeb.defaultAddress.hex).then((err, bandwidth) => {
-      console.log('Bandwidth after check', bandwidth);
-    });
-  });
+  }).catch(e=>console.error(e));
 }
 
 factoryContract.FaceWorthPollCreated().watch((err, data) => {
@@ -64,7 +54,7 @@ factoryContract.FaceWorthPollCreated().watch((err, data) => {
           check(hash, currentBlock.toNumber());
         } else if (currentStage === 2) {
           if (currentBlock >= checkPointTwo) {
-            check(hash, currentBlock.toNumber);
+            check(hash, currentBlock.toNumber());
           }
         } else {
           clearInterval(watcher);
@@ -78,14 +68,14 @@ factoryContract.StageChange().watch((err, data) => {
   if (err) return console.error('Failed to bind event listener:', err);
   if (data) {
     let { result } = data;
-    if (result.newStage === '5') {
+    if (result.newStage > '2') {
       factoryContract.getStatus(`0x${result.hash}`).call().then((data) => {
         update(
           result.hash,
           result.newStage,
-          data.totalWorth,
-          data.participantCount,
-          data.revealCount).catch(e => console.error(e));
+          data.totalWorth.toNumber(),
+          data.participantCount.toNumber(),
+          data.revealCount.toNumber()).catch(e => console.error(e));
       }).catch(e => {
         console.error(e);
       });
@@ -97,12 +87,37 @@ factoryContract.StageChange().watch((err, data) => {
   }
 });
 
+function updateEndedGames(games) {
+  return games.filter((game) => {
+    let hasError = false;
+    factoryContract.getStatus(`0x${game.hash}`).call().then((data) => {
+      console.log(game.hash, 'current stage is', data.currentStage);
+      if(data.currentStage > 2) {
+        update(
+          game.hash,
+          data.currentStage.toString(),
+          data.totalWorth.toNumber(),
+          data.participantCount.toNumber(),
+          data.revealCount.toNumber()).catch(e => {
+          console.error(e);
+          hasError = true;
+        });
+      }
+    }).catch(e => {
+      hasError = true;
+      console.error(e);
+    });
+    return hasError;
+  });
+}
+
 list({ 'status': '1' }, 1000).then((games) => {
   if (games && games.length > 0) {
     console.log(new Date(), 'found', games.length, 'games in commit stage');
+    const gamesInProgress = updateEndedGames(games);
     const checked = new Map();
     let maxCheckPoint = 0;
-    games.forEach((game) => {
+    gamesInProgress.forEach((game) => {
       let checkPoint = game.commitEndBlock + 1;
       if (!checked.has(checkPoint)) {
         checked.set(checkPoint, false);
@@ -114,7 +129,7 @@ list({ 'status': '1' }, 1000).then((games) => {
     let commitWatcher = setInterval(async () => {
       let currentBlock = await factoryContract.getCurrentBlock().call();
       let currentBlockNumber = currentBlock.toNumber();
-      games.forEach((game) => {
+      gamesInProgress.forEach((game) => {
         let checkPoint = game.commitEndBlock + 1;
         if(checkPoint <= currentBlockNumber && !checked.get(checkPoint)) {
           check(game.hash, currentBlockNumber);
@@ -131,9 +146,10 @@ list({ 'status': '1' }, 1000).then((games) => {
 list({ 'status': '2' }, 1000).then((games) => {
   if (games && games.length > 0) {
     console.log(new Date(), 'found', games.length, 'games in reveal stage');
+    const gamesInProgress = updateEndedGames(games);
     const checked = new Map();
     let maxCheckPoint = 0;
-    games.forEach((game) => {
+    gamesInProgress.forEach((game) => {
       let checkPoint = game.revealEndBlock + 1;
       if (!checked.has(checkPoint)) {
         checked.set(checkPoint, false);
@@ -145,7 +161,7 @@ list({ 'status': '2' }, 1000).then((games) => {
     let revealWatcher = setInterval(async () => {
       let currentBlock = await factoryContract.getCurrentBlock().call();
       let currentBlockNumber = currentBlock.toNumber();
-      games.forEach((game) => {
+      gamesInProgress.forEach((game) => {
         let checkPoint = game.revealEndBlock + 1;
         if(checkPoint <= currentBlockNumber && !checked.get(checkPoint)) {
           check(game.hash, currentBlockNumber);
